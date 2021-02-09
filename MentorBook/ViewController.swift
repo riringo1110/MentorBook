fdd40d06e6d717b4de703d3b9445e7afa3f3d007
Ririko Nagaishi <riry.cucumber@gmail.com>
2021-02-10 03:39:21 +0900

    メンターブック

--- MentorBook/ViewController.swift
+++ MentorBook/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var mentorArray: [Mentor] = []
     
     @IBOutlet var imageView:UIImageView!
@@ -19,12 +19,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.
         
         mentorArray.append(Mentor(name:"ながた", imageName: "nagata.jpg", course:"iPhone"))
         mentorArray.append(Mentor(name:"りょう", imageName: "ryo.jpg", course:"Unity"))
         mentorArray.append(Mentor(name:"たいてぃ", imageName: "taithi.jpg", course:"WebS,WebD"))
-    
+        
+        
         setUI()
         
     }
@@ -32,31 +33,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    func setUI(){
-        imageView.image = mentorArray[0].getImage()
-        nameLabel.text = mentorArray[0].name
-        courseLabel.text = mentorArray[0].course
-        
+    
+    
     func setUI(){
         imageView.image = mentorArray[index].getImage()
         nameLabel.text = mentorArray[index].name
         courseLabel.text = mentorArray[index].course
-    }
-    }
-    
-    
-    @IBAction func mae() {
-        index = index - 1
-        setUI()
-    }
-    
-    @IBAction func tugi() {
-        index = index + 1
-        setUI()
+       
+       
     }
 
+
+@IBAction func mae() {
+    index = index - 1
+    
+    if index > 2 {
+        index = 0
+    }
+        setUI()
+    
+    
 }
 
 
+@IBAction func tugi() {
+    index = index + 1
+    
+    if index > 2 {
+        index = 0
+    }
+        setUI()
+    
+}
+    
+}
 
 
